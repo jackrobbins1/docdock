2e3b3dc1a0d50b5d9d65b1738756aa2f8b9a318f
diautzi <dia_utzi@yahoo.com>
2019-05-28 13:43:27 -0500

    routes done

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,7 +2,7 @@ class Folder < ApplicationRecord
   belongs_to :user
   has_many :docs
 
-  has_many :subfolders, class_name: "Folder", foreign_key: "parent_folder_id"
-  belongs_to :parent_folder, class_name: "Folder"
+  has_many :subfolders, :class_name => "Folder", :foreign_key => "parent_folder"
+  belongs_to :parent_folder, :class_name => "Folder", :foreign_key => "parent_folder", optional: true
 
 end
