1bfbc25755ef17b4af291f13a6d6add4b35c9f6d
Jackson Robbins <jackrobbins1@gmail.com>
2019-05-28 15:11:36 -0500

    fixed merge conflicts from master

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,12 +2,8 @@ class Folder < ApplicationRecord
   belongs_to :user
   has_many :docs
 
-<<<<<<< HEAD
   has_many :subfolders, class_name: "Folder", foreign_key: "parent_folder_id"
   belongs_to :parent_folder, class_name: "Folder", foreign_key: "parent_folder_id", optional: true
-=======
-  has_many :subfolders, :class_name => "Folder", :foreign_key => "parent_folder"
-  belongs_to :parent_folder, :class_name => "Folder", :foreign_key => "parent_folder", optional: true
->>>>>>> master
+
 
 end
