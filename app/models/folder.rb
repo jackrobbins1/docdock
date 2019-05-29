f68aa29f2a40304cd9527d7d08b447fe1588c5eb
diautzi <dia_utzi@yahoo.com>
2019-05-29 16:13:46 -0500

    delete folders and added dependents on user and folders models

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,6 @@
 class Folder < ApplicationRecord
   belongs_to :user
-  has_many :docs
+  has_many :docs, dependent: :destroy
 
   has_many :subfolders, :class_name => "Folder", :foreign_key => "parent_folder"
   belongs_to :parent_folder, :class_name => "Folder", :foreign_key => "parent_folder", optional: true
